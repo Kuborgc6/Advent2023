644616642478ed1b0bd54c3dfc6592615d29018c
Jakub <kuborgc6@gmail.com>
2023-12-03 11:52:17 +0100

    Finnished Part Two of Day 2

--- day2.py
+++ day2.py
@@ -1,4 +1,4 @@
-import re
+import math
 
 file = open('input.txt', 'r')
 # file = open('input_test.txt', 'r')
@@ -10,6 +10,7 @@ condition_dict = {'red': 12, 'green': 13, 'blue': 14}
 max_balls = 12+13+14
 
 for line in lines:
+    balls_dict = {'red': 0, 'green': 0, 'blue': 0}
     condition_match = True
     game_id = int(line[1][:-1])
     taken = list()
@@ -25,17 +26,25 @@ for line in lines:
             temp.append(balls)
     taken.append(temp)
 
-    for take in taken:
-        value = 0
-        for i in range(int(len(take)/2)):
-            value += int(take[2*i])
-            if (condition_dict[take[2*i+1]] < int(take[2*i])) or value > max_balls:
-                condition_match = False
-                break
-        if condition_match == False:
-            break
+    # for take in taken:
+    #     value = 0
+    #     for i in range(int(len(take)/2)):
+    #         value += int(take[2*i])
+    #         if (condition_dict[take[2*i+1]] < int(take[2*i])) or value > max_balls:
+    #             condition_match = False
+    #             break
+    #     if condition_match == False:
+    #         break
     
-    if condition_match:
-        result += game_id
+    # if condition_match:
+    #     result += game_id
+
+    for take in taken:
+        for i in range(int(len(take)/2)):
+            if balls_dict[take[2*i+1]] < int(take[2*i]):
+                balls_dict[take[2*i+1]] = int(take[2*i])
+    result += math.prod(list(balls_dict.values())) 
+
+
 
 print(result)
