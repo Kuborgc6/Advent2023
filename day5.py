c51b68254ee5b464ed1cce04ca634bda1f96446f
Jakub <kuborgc6@gmail.com>
2023-12-05 15:11:10 +0100

    Working on part One

--- day5.py
+++ day5.py
@@ -17,4 +17,34 @@ for seed in lines[1][1:]:
     seeds.append(int(seed))
 
 print('test')
+what_now = ''
+for line in lines:
+    if line == "seed-to-soil map:":
+        what_now = "seed_to_soil"
+    elif line == "soil-to-fertilizer map:":
+        what_now = "soil_to_fertilizer"
+    elif line == "fertilizer-to-water map:":
+        what_now = "fertilizer_to_water"
+    elif line == "water-to-light map:":
+        what_now = "water_to_light"
+    elif line == "light-to-temperature map:":
+        what_now = "light_to_temperature"
+    elif line == "temperature-to-humidity map:":
+        what_now = "temperature_to_humidity"
+    elif line == "humidity-to-location map:":
+        what_now = "humidity_to_location"
 
+soil_to_fertilizer = dict()
+fertilizer_to_water = dict()
+water_to_light = dict()
+light_to_temperature = dict()
+temperature_to_humidity = dict()
+humidity_to_location = dict()
+
+
+def find_mapping(current_numbers, mapping):
+    for map in mapping:
+        list_temp = range(map[0],map[2])
+    if current_numbers
+    return current_numbers
+    
