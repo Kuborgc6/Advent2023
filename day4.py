36c48cc2052ac9b73ad41126fd903f5586b4a282
Jakub <kuborgc6@gmail.com>
2023-12-05 14:29:11 +0100

    Finnished Part Two of Day 4

--- day4.py
+++ day4.py
@@ -5,15 +5,22 @@ lines = [line.split() for line in file]
 
 print('test')
 result = 0
-for line in lines:
-    separator = line.index('|')
-    winning_numbers = line[2:separator]
-    my_numbers = line[separator+1:]
-    winning_numbers_set = set(winning_numbers) 
-    my_numbers_set = set(my_numbers) 
-    my_winning = winning_numbers_set.intersection(my_numbers_set)
-    if my_winning:
-        points = pow(2,len(my_winning)-1)
-        result += points
+how_many_scratchcards = dict()
+for i in range(len(lines)):
+    how_many_scratchcards[i] = 1
 
-print(result)
+for i in range(len(lines)):
+    for j in range(how_many_scratchcards[i]):
+        line = lines[i]
+        game = int(line[1][:-1])
+        separator = line.index('|')
+        winning_numbers = line[2:separator]
+        my_numbers = line[separator+1:]
+        winning_numbers_set = set(winning_numbers) 
+        my_numbers_set = set(my_numbers) 
+        my_winning = winning_numbers_set.intersection(my_numbers_set)
+        if my_winning:
+            for k in range(len(my_winning)):
+                how_many_scratchcards[game + k] += 1
+
+print(sum(how_many_scratchcards.values()))
